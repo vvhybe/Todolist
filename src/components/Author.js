8d1af33e1e2a0d6b5c82165f08cc1802e4e2c3d3
why be <whybe7@hotmail.com>
2022-10-11 11:24:01 +0100

    up: Author.jsx

--- src/components/Author.js
+++ src/components/Author.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Author() {
   return (
     <div className='author'>
-        <a className='signature' target="_blank" href='https://instagram.com/why.be7'>WhyBe7<span>7</span></a>
+        <a className='signature' target="_blank" rel="noreferrer" href='https://instagram.com/why.be7'>WhyBe7<span>7</span></a>
     </div>
   )
 }
